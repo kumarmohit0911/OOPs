a6986f9b5ac9bb79e6bf6f0d87528bbba7374b32
Kumar Mohit <kumarmohitsspn969@gmail.com>
2024-09-28 00:29:59 +0530

    Updated File

--- multiple_inheritance_HC.py
+++ multiple_inheritance_HC.py
@@ -3,7 +3,7 @@ let the ElectricCar class inherit from both,
 demostrating multiple inheritance.'''
 class Car:
     def __init__(self,brand,model):
-        self.__brand=brand
+        self.brand=brand
         self.model=model
     def full_name(self):
         return f"{self.brand} {self.model}"
@@ -11,20 +11,26 @@ class Car:
         return self.__brand + "!"
     def fuel_type(self):
         return "petrol or diesel"
-class ElectricCar(Car):
-    def __init__(self,brand,model,battery_size):
-        super().__init__(brand,model)
-        self.battery_size=battery_size 
-    def fuel_type(self):
-        return "Electric charge"
+
 class Battery:
+    def __init__(self,watt):
+        self.watt=watt
     def battery_info(self):
-        return "this is battery"
+        return f"this car has battery of {self.watt} ."
 class Engine:
+    def __init__(self,horse_power):
+        self.horse_power=horse_power
     def engine_info(self):
-        return "this is engine"
+        return f"this car has {self.horse_power} engine"
+
+        
 class ElectricCarTwo(Battery,Engine,Car):
-    pass
-my_new_tesla=ElectricCarTwo("Tesla","Model S")
+    def __init__(self, brand, model,watt,horse_power):
+        Car.__init__(self,brand,model)
+        Engine.__init__(self,horse_power)
+        Battery.__init__(self,watt)
+
+my_new_tesla=ElectricCarTwo("Tesla","Model S","160kwh","100 HP")
 print(my_new_tesla.engine_info())
+print(my_new_tesla.battery_info())
 
